0c99daff0fc640dd203abe92c80120a176842098
Guilherme Guerra <guilherme.ga@gmail.com>
2020-09-05 11:14:57 -0300

    Adds scss preprocessor and fix prettier config

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,39 @@
-import svelte from "rollup-plugin-svelte";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
+import svelte from 'rollup-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import livereload from 'rollup-plugin-livereload'
+import { terser } from 'rollup-plugin-terser'
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
+
+const preprocess = sveltePreprocess({
+  scss: {
+    includePaths: ['src'],
+  },
+  postcss: {
+    plugins: [require('autoprefixer')],
+  },
+})
 
 export default {
-  input: "src/main.js",
+  input: 'src/main.js',
   output: {
     sourcemap: true,
-    format: "iife",
-    name: "app",
-    file: "public/build/bundle.js"
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js',
   },
   plugins: [
     svelte({
       // enable run-time checks when not in production
       dev: !production,
+      preprocess,
       // we'll extract any component CSS out into
       // a separate file - better for performance
-      css: css => {
-        css.write("public/build/bundle.css");
-      }
+      css: (css) => {
+        css.write('public/build/bundle.css')
+      },
     }),
 
     // If you have external dependencies installed from
@@ -32,7 +43,7 @@ export default {
     // https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       browser: true,
-      dedupe: ["svelte"]
+      dedupe: ['svelte'],
     }),
     commonjs(),
 
@@ -42,30 +53,30 @@ export default {
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
-    !production && livereload("public"),
+    !production && livereload('public'),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
-    production && terser()
+    production && terser(),
   ],
   watch: {
-    clearScreen: false
-  }
-};
+    clearScreen: false,
+  },
+}
 
 function serve() {
-  let started = false;
+  let started = false
 
   return {
     writeBundle() {
       if (!started) {
-        started = true;
+        started = true
 
-        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-          stdio: ["ignore", "inherit", "inherit"],
-          shell: true
-        });
+        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true,
+        })
       }
-    }
-  };
+    },
+  }
 }
